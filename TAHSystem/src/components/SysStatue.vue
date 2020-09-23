d20fd5f37f2f7cc68902744cfce7fa731035a934
songhejun <wqshj@126.com>
2020-09-23 23:33:08 +0800

    update12

--- TAHSystem/src/components/SysStatue.vue
+++ TAHSystem/src/components/SysStatue.vue
@@ -1,10 +1,12 @@
 <template>
-    <div>系统状态</div>
-    <p><span>id：</span><span>{{id}}</span></p>
-    <p><span>进程名称：</span><span>{{processName}}</span></p>
-    <p><span>COM端口：</span><span>{{comName}}</span></p>
-    <p><span>进程id：</span><span>{{processId}}</span></p>
-    <el-button type="primary" @click="CloseProcess">关闭服务</el-button>
+    <div>
+        <p>系统状态</p>
+        <p><span>id：</span><span>{{id}}</span></p>
+        <p><span>进程名称：</span><span>{{processName}}</span></p>
+        <p><span>COM端口：</span><span>{{comName}}</span></p>
+        <p><span>进程id：</span><span>{{processId}}</span></p>
+        <el-button type="primary" @click="CloseProcess">关闭服务</el-button>
+    </div>
 </template>
 
 <script>
@@ -15,21 +17,23 @@
                 id: "",
                 comName: "",
                 processName: "",
-                processId: ""
+                processId: "",
+                res: {},
             }
         },
 
         methods: {              // 方法
             //绑定ElementUI current-change 事件 当分页页码改变的时候触发
-            GetPortName(val) {
+            GetRunningProcess() {
                 this.axios
                     .get('http://localhost:5000/api/Configure/GetRunningProcess')
                     .then((response) => {
                         console.log(response.data.serverData);
-                        id = response.data.serverData.id;
-                        comName = response.data.serverData.comName;
-                        processName = response.data.serverData.processName;
-                        processId = response.data.serverData.processId;
+                        this.res = response.data.serverData
+                        this.id = this.res.id;
+                        this.comName = this.res.comName;
+                        this.processName = this.res.processName;
+                        this.processId = this.res.processId;
 
 
                     }).catch((response) => {
@@ -47,6 +51,7 @@
                     .get('http://localhost:5000/api/Configure/KillProcess?id=' + this.id)
                     .then((response) => {
                         console.log(response.data.serverData);
+                   
                         var ok = response.data.data;
                         if (ok) {
                             alert("服务已经关闭");
@@ -61,10 +66,12 @@
                         console.log(response);
                     })
             },
-            created() {
-                this.GetPortName();
 
-            }
+        },
+        created: function () {
+            this.GetRunningProcess();
+            console.log("start ");
+
         }
     }
 </script>
